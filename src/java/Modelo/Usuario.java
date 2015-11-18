a3bad6a11b833c1b614ad3ae36871468b266a586
Jorge <jorgeluis.tanos@exitco.biz>
2015-11-18 02:23:08 -0300

    Alta Usuario

--- src/java/Modelo/Usuario.java
+++ src/java/Modelo/Usuario.java
@@ -6,16 +6,28 @@
 package Modelo;
 
 import Dao.BDUsuarios;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import org.primefaces.model.UploadedFile;
 /**
  *
  * @author Jorge
  */
-public class Usuario {
+public class Usuario implements Serializable {
 
-    private int id = 0;
+    private int id;
     private String nombre;
     private String apellido;
     private int dni;
@@ -26,174 +38,300 @@ public class Usuario {
     private String telefono;
     private String urlImg;
     private boolean logueado;
+    private List<Usuario> listaUsuarios;
+    private Usuario selectedUsuario;
+    private Usuario editarUsuario;
+    private UploadedFile uploadedFile;
+    private Usuario nuevoUsuario;
+    
+    private BDUsuarios datosUsuarios = BDUsuarios.getInstance();
 
     public boolean isLogueado() {
-	return logueado;
+        return logueado;
     }
 
     public void setLogueado(boolean logueado) {
-	this.logueado = logueado;
+        this.logueado = logueado;
     }
 
     public String getUrlImg() {
-	return urlImg;
+        return urlImg;
     }
 
     public void setUrlImg(String urlImg) {
-	this.urlImg = urlImg;
+        this.urlImg = urlImg;
     }
 
     public String getNombre() {
-	return nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
-	this.nombre = nombre;
+        this.nombre = nombre;
     }
 
     public String getApellido() {
-	return apellido;
+        return apellido;
     }
 
     public void setApellido(String apellido) {
-	this.apellido = apellido;
+        this.apellido = apellido;
     }
 
     public int getDni() {
-	return dni;
+        return dni;
     }
 
     public void setDni(int dni) {
-	this.dni = dni;
+        this.dni = dni;
     }
 
     public boolean isAdministrador() {
-	return administrador;
+        return administrador;
     }
 
     public void setAdministrador(boolean administrador) {
-	this.administrador = administrador;
+        this.administrador = administrador;
     }
 
     public String getUser() {
-	return user;
+        return user;
     }
 
     public void setUser(String user) {
-	this.user = user;
+        this.user = user;
     }
 
     public String getPass() {
-	return pass;
+        return pass;
     }
 
     public void setPass(String pass) {
-	this.pass = pass;
+        this.pass = pass;
     }
 
     public String getEmail() {
-	return email;
+        return email;
     }
 
     public void setEmail(String email) {
-	this.email = email;
+        this.email = email;
     }
 
     public String getTelefono() {
-	return telefono;
+        return telefono;
     }
 
     public void setTelefono(String telefono) {
-	this.telefono = telefono;
+        this.telefono = telefono;
     }
 
     public int getId() {
-	return id;
+        return id;
     }
 
     public void setId(int id) {
-	this.id = id;
+        this.id = id;
+    }
+
+    public List<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public Usuario getSelectedUsuario() {
+        return selectedUsuario;
+    }
+
+    public void setSelectedUsuario(Usuario selectedUsuario) {
+        this.selectedUsuario = selectedUsuario;
+    }
+
+    public Usuario getEditarUsuario() {
+        return editarUsuario;
+    }
+
+    public void setEditarUsuario(Usuario editarUsuario) {
+        this.editarUsuario = editarUsuario;
+    }
+
+    public UploadedFile getUploadedFile() {
+        return uploadedFile;
+    }
+
+    public void setUploadedFile(UploadedFile uploadedFile) {
+        this.uploadedFile = uploadedFile;
+    }
+
+    public Usuario getNuevoUsuario() {
+        return nuevoUsuario;
+    }
+
+    public void setNuevoUsuario(Usuario nuevoUsuario) {
+        this.nuevoUsuario = nuevoUsuario;
     }
 
     public Usuario(int id, String nombre, String apellido, int dni, boolean administrador, String user, String pass, String email, String telefono, String urlImg) {
-	this.id = id;
-	this.nombre = nombre;
-	this.apellido = apellido;
-	this.dni = dni;
-	this.administrador = administrador;
-	this.user = user;
-	this.pass = pass;
-	this.email = email;
-	this.telefono = telefono;
-	this.urlImg = urlImg;
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.administrador = administrador;
+        this.user = user;
+        this.pass = pass;
+        this.email = email;
+        this.telefono = telefono;
+        this.urlImg = urlImg;
     }
 
-    public Usuario() {
-	this.logueado = false;
+    public Usuario() throws SQLException {
+        this.logueado = false;
+        this.listaUsuarios = datosUsuarios.listado();
+        nuevoUsuario = new Usuario("");        
     }
 
-    public Usuario(String user, String pass) {
-	this.user = user;
-	this.pass = pass;
-	this.id = 0;
-	this.nombre = "";
-	this.apellido = "";
-	this.dni = 0;
-	this.administrador = false;
-	this.email = "";
-	this.telefono = "";
+    public Usuario(String edit) {
+        this.logueado = false;
     }
 
-    private BDUsuarios datosUsuarios = BDUsuarios.getInstance();
+    public Usuario(String user, String pass) throws SQLException {
+        this.user = user;
+        this.pass = pass;
+        this.id = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.dni = 0;
+        this.administrador = false;
+        this.email = "";
+        this.telefono = "";
+    }
+
+    
 
     public void registraUsuario(Usuario user) throws SQLException {
-	datosUsuarios.alta(user);
+        datosUsuarios.alta(user);
     }
 
     public void modificarUsuario(Usuario user) throws SQLException {
-	datosUsuarios.modificar(user);
-    }
-
-    public void bajaUsuario(int id) throws SQLException {
-	datosUsuarios.baja(datosUsuarios.traePorId(id));
+        datosUsuarios.modificar(user);
     }
 
     public ArrayList listarUsuarios() throws SQLException {
-	return datosUsuarios.listado();
+        return datosUsuarios.listado();
     }
 
     public Usuario traePorId(int id) throws SQLException {
-	return datosUsuarios.traePorId(id);
+        return datosUsuarios.traePorId(id);
     }
 
     public Usuario validaSIExiste(String user, String email) throws SQLException {
-	return datosUsuarios.validaUser(user, email);
+        return datosUsuarios.validaUser(user, email);
     }
 
     public String validaUsuario() throws SQLException {
-	Usuario validado = null;
-	validado = (Usuario) datosUsuarios.existe(new Usuario(this.user, this.pass));
-	if (validado != null) {
-	    this.nombre = validado.nombre;
-	    this.apellido = validado.apellido;
-	    this.dni = validado.dni;
-	    this.email = validado.email;
-	    this.id = validado.id;
-	    this.user = validado.user;
-	    this.pass = validado.pass;
-	    this.telefono = validado.telefono;
-	    this.urlImg = validado.urlImg;
-	    this.administrador = validado.administrador;
-	    this.logueado = true;
-	}
-	//else
-	// this.logueado = false;
-	return "log";
+        System.getProperties().put("org.apache.el.parser.COERCE_TO_ZERO", "false");
+        Usuario validado = null;
+        validado = (Usuario) datosUsuarios.existe(new Usuario(this.user, this.pass));
+        if (validado != null) {
+            this.nombre = validado.nombre;
+            this.apellido = validado.apellido;
+            this.dni = validado.dni;
+            this.email = validado.email;
+            this.id = validado.id;
+            this.user = validado.user;
+            this.pass = validado.pass;
+            this.telefono = validado.telefono;
+            this.urlImg = validado.urlImg;
+            this.administrador = validado.administrador;
+            this.logueado = true;
+        }
+        //else
+        // this.logueado = false;
+        return "log";
     }
 
     public String desLoguear() {
-	this.logueado = false;
-	return "nolog";
+        this.logueado = false;
+        return "nolog";
     }
 
+    public String listar() throws Exception {
+        listaUsuarios = new ArrayList();
+        this.listaUsuarios = datosUsuarios.listado();
+        return "listarUsuarios";
+    }
+
+    public String altaUsuario() throws SQLException{
+        if (this.uploadedFile != null) {
+            if (!this.uploadedFile.getFileName().equals("")) {
+                uploadFile(this);
+                this.urlImg = this.uploadedFile.getFileName();
+            } else {
+                this.urlImg = "user-default.png";
+            }
+        }
+        datosUsuarios.alta(this);
+        alta();
+        return "AltaUsuario";
+    }
+
+    public String modificaUsuario() throws Exception {
+        Usuario userBd = datosUsuarios.traePorId(this.editarUsuario.id);
+        if (!this.editarUsuario.uploadedFile.getFileName().equals("")) {
+            uploadFile(this.editarUsuario);
+            this.editarUsuario.urlImg = this.editarUsuario.uploadedFile.getFileName();
+        } else {
+            if (userBd != null && userBd.urlImg == null) {
+                this.editarUsuario.urlImg = "user-default.png";
+            }
+        }
+        if (userBd != null) {
+            datosUsuarios.modificar(this.editarUsuario);
+        }
+        this.editarUsuario = null;
+        return "EditaUsuario";
+    }
+
+    public void uploadFile(Usuario user) {
+        try {
+            HttpServletRequest request
+                    = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext().getRequest();
+            String dir = request.getRealPath("/");
+            String dir2 = dir.replaceAll("web", "img");
+            String dir3 = dir2.replaceAll("build", "web");
+            dir3 = dir3.concat("imgUsuarios/");
+            String filename = user.uploadedFile.getFileName();
+            Path folder = Paths.get(dir3, filename);
+            Path file = Files.createFile(folder);
+            InputStream input = user.uploadedFile.getInputstream();
+            Files.copy(input, file, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            Logger.getLogger(Usuario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String bajaUsuario() throws Exception {
+        datosUsuarios.baja(datosUsuarios.traePorId(this.editarUsuario.id));
+        return "EliminaUsuario";
+    }
+
+    public void alta() throws SQLException {        
+        this.user = "";
+        this.pass = "";
+        this.id = 0;
+        this.nombre = "";
+        this.apellido = "";
+        this.dni = 0;
+        this.administrador = false;
+        this.email = "";
+        this.telefono = "";
+    }
+
+    public void editar() {
+        this.editarUsuario = this.selectedUsuario;
+    }
+
+    public void eliminar() {
+        this.editarUsuario = this.selectedUsuario;
+    }
+  
 }
